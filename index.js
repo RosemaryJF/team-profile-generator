c13131d3267807139d056250a69b33adab2984fc
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-09-30 16:06:20 +1000

    Questions for each team member created

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+const Employee = require("./employee")
+const Engineer= require("./engineers")
+const Intern = require("./intern")
+const Manager = require("./manager")
