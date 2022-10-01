223b7f5536dd01092066584a2e5f1a37d8c3b296
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-10-01 18:17:39 +1000

    All questions added and answers are logging into an Employee Array

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # team-profile-generator
 
 https://jestjs.io/docs/asynchronous
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
--- index.js
+++ index.js
@@ -1,36 +1,90 @@
 const inquirer = require("inquirer");
 const jest = require("jest");
 const fs = require("fs");
+const htmlTemplate = require("./src/html-page-template")
 
-const { Employee, employeePrompt } = require("./lib/Employee");
-const { Engineer, engineerQuestions } = require("./lib/Engineer");
-const { Intern, internQuestions } = require("./lib/Intern");
-const { Manager, managerQuestions } = require("./lib/Manager");
+const {managerQuestions, Manager} = require("./lib/Manager");
+const {engineerQuestions, Engineer} = require("./lib/Engineer");
+const {internQuestions, Intern} = require("./lib/Intern");
 
 const employeesArr = [];
 
-const init = () => { managerQuestionFunc() }
+const init = () => { managerQuestionsFunc() }
 
-const managerQuestionFunc = () => {
+const managerQuestionsFunc = () => {
     inquirer.prompt(managerQuestions)
         .then((answers) => {
-            answers = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+            answers = new Manager(
+                answers.managerName,
+                answers.managerName,
+                answers.managerName,
+                answers.managerName)
             employeesArr.push(answers);
             return addToTeamPrompt();
-        })
-}
+        });
+};
+
+const engineerQuestionsFunc = () => {
+    inquirer.prompt(engineerQuestions)
+        .then((answers) => {
+            answers = new Engineer(
+                answers.engineerName,
+                answers.engineerid,
+                answers.engineerEmail,
+                answers.engineerGithub)
+            employeesArr.push(answers);
+            return addToTeamPrompt();
+        });
+};
+
+const internQuestionsFunc = () => {
+    inquirer.prompt(internQuestions)
+        .then((answers) => {
+            answers = new Intern(
+                answers.internName,
+                answers.internId,
+                answers.internEmail,
+                answers.internSchool)
+            employeesArr.push(answers);
+            return addToTeamPrompt();
+        });
+};
 
 const addToTeamPrompt = () => {
     inquirer.prompt(employeePrompt)
         .then(answer => {
-            if (answer.teamMemberType === "Engineer") {
-                    console.log("You've chosen to add an Engineer to your team!");
-            } else if (answer.teamMemberType === "Intern") {
-                    console.log("You've chosen to add an Intern to your team!");
+            if (answer.employeeType === "Engineer") {
+                engineerQuestionsFunc();
+            } else if (answer.employeeType === "Intern") {
+                internQuestionsFunc();
             } else {
-                    console.log("You've finished building your team!");
+                console.log(employeesArr);
+                // generateHTML();
             };
         });
 };
+
+const employeePrompt = [
+    {
+        type: 'list',
+        message: "Which type of team member would you like to add?",
+        name: "employeeType",
+        choices: [
+            {name: 'Engineer'},
+            {name: 'Intern'}, 
+            {name: "I don't want to add any more team members"}
+        ],
+    },
+];
+
+// const generateHTML = () => {
+//     let htmlFile = htmlTemplate(employeesArr)
+//                 fs.writeFile("index.html", htmlFile, (err) =>
+//                     err ? console.error(err) : console.log("Congrats! Your team")
+//                 )
+//                 .catch((err) => {
+//                 console.error(err)
+//             });
+// };
 
 init()
