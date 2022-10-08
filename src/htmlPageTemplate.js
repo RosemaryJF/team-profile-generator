e3f445f41ce7e8a590d0b06b5ac2abf450f88cda
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-10-08 16:02:50 +1100

    more CSS tidying

--- src/htmlPageTemplate.js
+++ src/htmlPageTemplate.js
@@ -6,9 +6,9 @@ const generateEmployeeCards = employees => {
         return `
             <div class="tile is-child is-4 has-text-centered employeeCard">
                 <h2>${managerAnswers.getName()}</h2>
-                <i class="fas fa-tasks" style="color: #ffffff;"></i><h3>${managerAnswers.getRole()}</h3>
+                <h3><i class="fas fa-tasks" style="color: #ffffff;"></i>${managerAnswers.getRole()}</h3>
                 <p>ID: ${managerAnswers.getID()}</p>
-                <p>Email: ${managerAnswers.getEmail()}</p>
+                <p>Email: <a href="mailto:${managerAnswers.getEmail()}">${managerAnswers.getEmail()}</a></p>
                 <p>Office Number: ${managerAnswers.getOfficeNumber()}</p>
             </div>
     `
@@ -18,9 +18,9 @@ const generateEmployeeCards = employees => {
         return `
             <div class="tile is-child is-4 has-text-centered">
                 <h2>${engineerAnswers.getName()}</h2>
-                <i class="fas fa-user-cog" style="color: #ffffff;"></i><h3>${engineerAnswers.getRole()}</h3>
+                <h3><i class="fas fa-user-cog" style="color: #ffffff;"></i>${engineerAnswers.getRole()}</h3>
                 <p>ID: ${engineerAnswers.getID()}</p>
-                <p>Email: ${engineerAnswers.getEmail()}</p>
+                <p>Email: <a href="mailto:${engineerAnswers.getEmail()}">${engineerAnswers.getEmail()}</a></p>
                 <p>GitHub: <a href="https://www.github.com/${engineerAnswers.getGitHub()}" target="_blank">${engineerAnswers.getGitHub()}</a></p>
             </div>
     `
@@ -29,11 +29,11 @@ const generateEmployeeCards = employees => {
     const generateInternCard = internAnswers => {
         return `
             <div class="tile is-child is-4 has-text-centered">
-            <h2>${internAnswers.getName()}</h2>
-            <i class="fas fa-user-graduate" style="color: #ffffff;"></i>${internAnswers.getRole()}</h3>
-            <p>ID: ${internAnswers.getID()}</p>
-            <p>Email: ${internAnswers.getEmail()}</p>
-            <p>School: ${internAnswers.getSchool()}</p>
+                <h2>${internAnswers.getName()}</h2>
+                <h3><i class="fas fa-user-graduate" style="color: #ffffff;"></i>${internAnswers.getRole()}</h3>
+                <p>ID: ${internAnswers.getID()}</p>
+                <p>Email: <a href="mailto:${internAnswers.getEmail()}">${internAnswers.getEmail()}</a></p>
+                <p>School: ${internAnswers.getSchool()}</p>
             </div>
     `
     }
